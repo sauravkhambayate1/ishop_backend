7e0c3305b75e57a7f40b0725b021af75772e6e36
sauravkhambayate1 <sauravkhambayate122@gmail.com>
2023-05-29 19:06:21 +0530

    changes saved

--- router/sign.js
+++ router/sign.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const route=express();
-const signController=require('../controller/sign');
+const signController=require('../controller/signIn');
 route.post('/signup',signController.signUpController);
 route.post('/signin',signController.signinController);
 route.get('/signin',signController.loginController);
