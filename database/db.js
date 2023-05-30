cda6ab4ac95a61bdf5339eab0b54816a5da6aa65
sauravkhambayate1 <sauravkhambayate122@gmail.com>
2023-05-30 12:27:52 +0530

    changes saved

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const mongo=require('mongoose')
-const url="mongodb+srv://sauravkhambayate221:Saurav123@cluster0.d9nult5.mongodb.net/Bazaar"
+const url="mongodb+srv://sauravkhambayate221:Saurav123@cluster0.d9nult5.mongodb.net/Bazaar?retryWrites=true&w=majority"
 
 const  connect=async()=>{
     try{
